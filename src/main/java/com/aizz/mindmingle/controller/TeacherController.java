33220a5a53c60cd797db4d700dd6d3f85ce8be36
zhangyuliang <zhangyuliang94@126.com>
2023-11-23 07:34:38 +0800

    引入web

--- src/main/java/com/aizz/mindmingle/controller/TeacherController.java
+++ src/main/java/com/aizz/mindmingle/controller/TeacherController.java
@@ -1,11 +1,21 @@
 package com.aizz.mindmingle.controller;
 
+import cn.hutool.http.HttpResponse;
 import com.aizz.mindmingle.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/teacher")
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
+    @GetMapping("/list")
+    public String list(){
+        return "test";
+    }
 }
