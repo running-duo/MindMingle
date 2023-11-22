4da9ac8ded6f06b51c7eb731944e11f5d36a4779
zxj <775718540@qq.com>
2023-11-22 10:18:59 +0800

    first commit

---
+++ src/main/java/com/aizz/mindmingle/MindMingleApplication.java
@@ -0,0 +1,13 @@
+package com.aizz.mindmingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindMingleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aizz/mindmingle/MindMingleApplicationTests.java
@@ -0,0 +1,55 @@
+package com.aizz.mindmingle;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMingleApplicationTests {
+
+    /**
+     * 数据源配置
+     */
+    private static final DataSourceConfig DATA_SOURCE_CONFIG = new DataSourceConfig
+            .Builder("jdbc:mysql://81.69.252.112:3306/mind_mingle?serverTimezone=Asia/Shanghai"
+            , "root", "123456")
+            .build();
+
+    @Test
+    void contextLoads() {
+    }
+
+    /**
+     * mybatis p 代码生成
+     */
+    @Test
+    void generateMapper() {
+        AutoGenerator generator = new AutoGenerator(DATA_SOURCE_CONFIG);
+        GlobalConfig globalConfig = new GlobalConfig.Builder().author("shiori").build();
+        StrategyConfig strategyConfig = new StrategyConfig.Builder()
+                .enableCapitalMode()
+                .enableSkipView()
+                .disableSqlFilter()
+                .addInclude("bis_all_fire_alarm_file")
+                .entityBuilder()
+                .enableLombok()
+                .enableFileOverride()
+                .build();
+        PackageConfig packageConfig = new PackageConfig.Builder()
+                .parent("com.aizz")
+                .entity("model")
+                .service("service")
+                .serviceImpl("service.impl")
+                .mapper("com/aizz/mapper")
+                .controller("controller")
+                .build();
+        generator.strategy(strategyConfig);
+        generator.global(globalConfig);
+        generator.packageInfo(packageConfig);
+        generator.execute();
+    }
+}
