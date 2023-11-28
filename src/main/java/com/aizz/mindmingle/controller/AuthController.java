983f01a2029445c2da24bf9844f48a9f9f270fd5
zhangyuliang <zhangyuliang94@126.com>
2023-11-28 14:15:51 +0800

    引入jasypt

--- src/main/java/com/aizz/mindmingle/controller/AuthController.java
+++ src/main/java/com/aizz/mindmingle/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.aizz.mindmingle.controller;
 
 import com.aizz.mindmingle.common.Response;
+import com.aizz.mindmingle.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/login")
     public Response<Void> login(){
+        authService.login();
         return Response.success();
     }
 }
--- src/main/java/com/aizz/mindmingle/service/impl/AuthServiceImpl.java
+++ src/main/java/com/aizz/mindmingle/service/impl/AuthServiceImpl.java
@@ -1,10 +1,8 @@
 package com.aizz.mindmingle.service.impl;
 
 import com.aizz.mindmingle.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 授权接口实现层
@@ -13,23 +11,20 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class AuthServiceImpl implements AuthService {
-
-    @Autowired
-    private RestTemplate restTemplate;
-    @Value("${}")
+    @Value("${miniProgram.appId}")
     private String appId;
-    @Value("${}")
+    @Value("${miniProgram.appSecret}")
     private String appSecret;
 
     private final String wechatServerUrl = "https://api.weixin.qq.com/sns/jscode2session";
 
     @Override
     public void login() {
-
+        miniProgramLogin("");
     }
 
-    private void miniProgramLogin(String code){
-        String url = wechatServerUrl + "?appId="+appId+"&secret="+appSecret+"&js_code="+code+"&grant_type=authorization_code";
+    private void miniProgramLogin(String code) {
+        String url = wechatServerUrl + "?appId=" + appId + "&secret=" + appSecret + "&js_code=" + code + "&grant_type=authorization_code";
 //        restTemplate.exchange()
     }
 }
