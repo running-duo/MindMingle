e4b0450b99d51ec158983f5e6a416993a678716f
zhangyuliang <your-email@example.com>
2025-08-02 18:07:39 +0800

    feat:加入bark推送

--- src/main/java/com/aizz/mindmingle/schedule/SignSchedule.java
+++ src/main/java/com/aizz/mindmingle/schedule/SignSchedule.java
@@ -11,6 +11,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -30,6 +33,8 @@ public class SignSchedule {
     @Scheduled(cron = "0 0 7 * * ?")
     private void cpSign() {
         log.info("准备签到...");
+        Boolean result = false;
+        Integer score = 0;
         try {
             // 构建请求头
             HttpHeaders headers = new HttpHeaders();
@@ -47,14 +52,54 @@ public class SignSchedule {
             log.info("发起签到...");
             JSONObject response = restTemplate.postForObject(url, requestEntity, JSONObject.class);
             log.info("签到响应:{}", response);
+            // {"user_id":1033,"created_at":"2025-08-01T00:41:17.086Z","id":116290,"value":5}
             // TODO:签到失败推送微信公众号
+            result = true;
+            score = response.getInteger("value");
         } catch (Exception ex) {
             log.error("cpSign error", ex);
+            result = false;
+            score = 0;
         }
         log.info("签到结束!");
+        // bark推送
+        barkNotice(result, score);
     }
 
-    @Scheduled(cron = "0 0 10 * * ?")
+    private void barkNotice(Boolean result, Integer score) {
+        try {
+            String url = "https://api.day.app/kcua6Td4HZP2d9pgv73B24";
+            StringBuilder body = new StringBuilder();
+            if (result) {
+                body.append("签到成功!\n时间：");
+            } else {
+                body.append("签到失败!\n时间：");
+            }
+            body.append(getCurrentFormattedTime());
+            body.append("\n获得积分：");
+            body.append(score);
+            // 构建请求头
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Content-Type", "application/json");
+            // 构建求体参数
+            HashMap<String, String> map = new HashMap<>();
+            map.put("body", body.toString());
+            map.put("title", "CP小程序打卡结果");
+            map.put("badge", "1");
+            map.put("sound", "minuet");
+            map.put("icon", "https://minio.xiaofeng.show/blog/2025/cowave-log.png");
+            map.put("group", "Cowave");
+            map.put("url", "");
+            // 组合请求头与请求体参数
+            HttpEntity<String> requestEntity = new HttpEntity<>(JSONObject.toJSONString(map), headers);
+            JSONObject response = restTemplate.postForObject(url, requestEntity, JSONObject.class);
+            log.info("bark response:{}", response);
+        } catch (Exception ex) {
+            log.error("barkNotice error", ex);
+        }
+    }
+
+    //    @Scheduled(cron = "0 0 10 * * ?")
     private void nfVideoSign() {
         log.info("准备nf.video签到...");
         try {
@@ -96,5 +141,21 @@ public class SignSchedule {
         } catch (Exception ex) {
             log.error("nfVideoSignCore error", ex);
         }
+    }
+
+    /**
+     * 获取当前时间并格式化为 "yyyy-MM-dd HH:mm:ss"
+     *
+     * @return 格式化后的时间字符串
+     */
+    private String getCurrentFormattedTime() {
+        // 获取当前日期和时间
+        LocalDateTime now = LocalDateTime.now();
+
+        // 定义日期时间格式
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // 格式化当前日期和时间
+        return now.format(formatter);
     }
 }
